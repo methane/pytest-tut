bc7c081b42aad3ba33372ca38f5e1e75d2b2ae8e
INADA Naoki <inada-n@klab.com>
2013-01-08 15:04:04 +0900

    share tests with function.

--- messege_filter_test.py
+++ messege_filter_test.py
@@ -1,12 +1,14 @@
 from messege_filter import MessageFilter
 
-def test_single_argument():
-    filter = MessageFilter('foo')
+def check_foo(filter):
     assert filter.detect("hello from foo"), "should detect message with NG word."
     assert not filter.detect("hello, world!"), "should not detect message without NG word."
+
+def test_single_argument():
+    filter = MessageFilter('foo')
+    check_foo(filter)
 
 def test_multiple_argument():
     filter = MessageFilter('foo', 'bar')
     assert filter.detect('hello from bar')
-    assert filter.detect("hello from foo"), "should detect message with NG word."
-    assert not filter.detect("hello, world!"), "should not detect message without NG word."
+    check_foo(filter)
